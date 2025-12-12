c513277603ffe1643b0b9db97f0042eb8feb833e
Gattuoch <gchambang@gmail.com>
2025-12-12 19:10:37 +0300

    deploy vercel

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -29,7 +29,13 @@ dotenv.config();
 const app = express();
 
 // ====== MIDDLEWARES ======
-app.use(cors());
+app.use(cors(
+  { origin: "*",
+    methods: ["GET","POST","PUT","DELETE","PATCH","OPTIONS"],
+    allowedHeaders: ["Content-Type","Authorization"],
+    credentials:true,
+  }
+));
 app.use(express.json());
 app.use(helmet());
 app.use(morgan("dev"));
