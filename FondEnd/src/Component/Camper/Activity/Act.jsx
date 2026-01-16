f5581a38fbfa00e8fefd259f67cbf2c05930bb64
Gattuoch <gchambang@gmail.com>
2026-01-16 18:35:40 +0300

    all change here

---
+++ FondEnd/src/Component/Camper/Activity/Act.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Act = () => {
+  return (
+    <div>Act</div>
+  )
+}
---
+++ FondEnd/src/Component/Camper/Bookings/Booki.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Booki = () => {
+  return (
+    <div>Booki</div>
+  )
+}
